02f7f5f32464c5b4c854e301f230fe5b7d4f3c41
Josep Oliver <josepoliverv@gmail.com>
2023-11-09 10:37:17 +0100

    test1

--- nivel1.c
+++ nivel1.c
@@ -1,2 +1,17 @@
 /* NIVEL 1 */
 #include "nivel1.h"
+
+//Método main
+int main() {
+
+    char line[COMMAND_LINE_SIZE];
+
+    while (read_line(line)) {
+
+        execute_line(line);
+    }
+
+    return 0;
+}
+
+
